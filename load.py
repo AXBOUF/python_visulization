4ac7e84af727fb20ab9b8cb36f68216035c03be5
Munal <165521222+AXBOUF@users.noreply.github.com>
2025-10-08 08:49:47 +0000

    added color and time

--- load.py
+++ load.py
@@ -17,7 +17,7 @@ def ascii_progress_bar(current, total, bar_length=10):
         filled_length = 0
     else:
         filled_length = int(round(bar_length * current / float(total)))
-    bar = '#' * filled_length + '-' * (bar_length - filled_length)
+    bar = '\033[92m' + '#' * filled_length + '\033[0m' + '\033[91m' + '-' * (bar_length - filled_length) + '\033[0m'
     return f"{current} out of {total} [{bar}]"
 
 
@@ -41,18 +41,39 @@ def ascii_progress_bar_time(elapsed, total, bar_length=20):
         filled_length = 0
     else:
         filled_length = int(round(bar_length * elapsed / float(total)))
-    bar = '#' * filled_length + '-' * (bar_length - filled_length)
-    elapsed_minutes = elapsed // 60
+    bar = '\033[92m' + '#' * filled_length + '\033[0m' + '\033[91m' + '-' * (bar_length - filled_length) + '\033[0m'
     total_minutes = total // 60
     return f"{elapsed_minutes} min out of {total_minutes} min [{bar}]"
 
+# given time format hh:mm:ss we can convert it to seconds
+# and then use the above function to show the progress bar
+def time_to_seconds(time_str):
+    h, m, s = map(int, time_str.split(':'))
+    return h * 3600 + m * 60 + s
+
 def promtuser_time():
     try:
-        elapsed = int(input("Enter the elapsed time in seconds: "))
-        total = int(input("Enter the total time in seconds: "))
+        elapsed_str = input("Enter the elapsed time (hh:mm:ss): ")
+        total_str = input("Enter the total time (hh:mm:ss): ")
         bar_length = int(input("Enter the desired length of the progress bar (default is 20): ") or 20)
+        elapsed = time_to_seconds(elapsed_str)
+        total = time_to_seconds(total_str)
         print(ascii_progress_bar_time(elapsed, total, bar_length))
     except ValueError:
-        print("Please enter valid integers for elapsed time, total time, and bar length.")
+        print("Please enter valid time in hh:mm:ss format and integers for bar length.")        
 
-promtuser_time()
+def main():
+    print("Choose an option:")
+    print("1. Progress bar based on current and total values")
+    print("2. Progress bar based on elapsed and total time")
+    choice = input("Enter 1 or 2: ")
+    if choice == '1':
+        promtuser()
+    elif choice == '2':     
+        promtuser_time()
+if __name__ == "__main__":
+    main()
+        
+# next we can add colors to the progress bar using ANSI escape codes
+# for example we can make the filled part green and the unfilled part red
+# this would make it more visually appealing    
